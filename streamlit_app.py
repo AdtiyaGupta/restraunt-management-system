177493625921d04a2799c152a4d3bb20632c0107
Aditya Gupta <155718055+AdtiyaGupta@users.noreply.github.com>
2024-08-06 11:10:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,17 @@ if 'menu' not in st.session_state:
         'Snacks': []
     }
 
+# Function to view menu
+def view_menu(category):
+    menu_df = pd.DataFrame(st.session_state.menu[category])
+    st.write(menu_df)
+
 # Main application
 st.sidebar.title("Restaurant Management System")
 
 # Menu bar
-selected_menu = st.sidebar.selectbox("Select Menu", ['Beverages', 'Chinese', 'North Indian', 'Sweets', 'South Indian', 'Snacks'])
-
+selected_menu = st.sidebar.selectbox("Select Menu", list(st.session_state.menu.keys()))
 
 # Main content
 st.title("Menu")
 view_menu(selected_menu)
-
-st.title("Orders")
-view_orders()
